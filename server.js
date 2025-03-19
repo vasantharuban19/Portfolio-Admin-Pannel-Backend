af2248db85ebb28b2e7d86744d44b8a46fe422e9
Vasantharuban S <vasanthruban1920@gmail.com>
2025-03-19 20:06:23 +0530

    updated

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import app from "./app.js";
-import cloudinary from 'cloudinary'
+import cloudinary from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config/config.env" });
 
 cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server Listening at port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Listening at port ${PORT}`);
 });
